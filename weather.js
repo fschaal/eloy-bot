33e7d5d928d25d601b134ec82470663395529d29
fschaal <florian.keekje@gmail.com>
2016-02-13 15:07:55 +0100

    Added weather icons.

--- weather.js
+++ weather.js
@@ -1,17 +1,20 @@
 var request = require('request')
 
-module.exports = function (token) {
+module.exports = function(token) {
   return new Weather(token)
 }
 
-function Weather (token) {
+function Weather(token) {
   var self = this
   self.token = token
 
-  self.get = function (location, fn) {
+  self.get = function(location, fn) {
     var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location +
-              '&units=metric&APPID=' + self.token
-    request({url: url, json: true}, function (error, response, data) {
+      '&units=metric&APPID=' + self.token
+    request({
+      url: url,
+      json: true
+    }, function(error, response, data) {
       if (error) {
         return fn(error)
       }
@@ -20,8 +23,30 @@ function Weather (token) {
       }
 
       var currentConditions = data.weather[0].description
-      var currentTemp = Math.round(data.main.temp * 1.8 + 32) + '°F'
-      var msg = 'The weather in *' + data.name + '* is *' + currentTemp + '* and *' + currentConditions + '*.'
+      var currentConditionsMain = data.weather[0].main
+      var currentTemp = data.main.temp + '°C'
+      var conditionIcon = ''
+      console.log(currentConditionsMain);
+      switch (currentConditionsMain.toLowerCase()) {
+        case 'clouds':
+          conditionIcon = ':cloud:'
+          break;
+        case 'rain':
+          conditionIcon = ':rain_cloud:'
+          break;
+        case 'clear':
+          conditionIcon = ':sunny:'
+          break;
+        case 'thunderstorm':
+          conditionIcon = ':thunder_cloud_and_rain:'
+          break;
+        case 'snow':
+          conditionIcon = ':snow_cloud:'
+          break;
+        default:
+
+      }
+      var msg = 'The weather in *' + data.name + '* is *' + currentTemp + '* and *' + currentConditions + conditionIcon + '*.'
       fn(null, msg)
     })
   }
